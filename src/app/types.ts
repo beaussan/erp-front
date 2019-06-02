3224e7cf893d7986fb72e6bb024264400ca18834
Nicolas Beaussart <nic.beaussart@gmail.com>
2019-06-02 16:49:06 +0200

    :ambulance: fix compilation error

---
+++ src/app/types.ts
@@ -0,0 +1,16 @@
+export class Master {
+  maquettes: string[];
+  _id: string;
+  name: string;
+}
+
+/*
+"maquettes": [
+        "5cf3da573839b7199778af1c"
+      ],
+      "_id": "5cf3da3c3839b7199778af1b",
+      "name": "Master III",
+      "description": "Un master en alternance informatique",
+      "__v": 0,
+      "id": "5cf3da3c3839b7199778af1b"
+ */
